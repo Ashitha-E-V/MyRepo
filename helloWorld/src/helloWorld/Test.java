34887da2da41a0c3ec8dd191a53868d7f694d40c
Ashitha <ashithachaitra@gmail.com>
2018-07-28 17:35:58 +0530

    added a new class to test branch

--- helloWorld/src/helloWorld/Test.java
+++ helloWorld/src/helloWorld/Test.java
@@ -1,10 +1,11 @@
 package helloWorld;
-
+import helloWorld.testclass;
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		testclass.testBranch();
 	}
 
 }
---
+++ helloWorld/src/helloWorld/testclass.java
@@ -0,0 +1,7 @@
+package helloWorld;
+
+public class testclass {
+	public static void testBranch(){
+		System.out.println("testBranch");
+	}
+}
